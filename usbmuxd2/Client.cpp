5919fa7e29b5485d1e566da96419894e7dea47a1
Jackson Coxson <admin@jkcoxson.com>
2022-03-22 09:05:40 -0600

    Close fd on error

--- usbmuxd2/Client.cpp
+++ usbmuxd2/Client.cpp
@@ -67,6 +67,7 @@ void Client::loopEvent() {
         recv_data();
     } catch (tihmstar::exception &e) {
         error("failed to recv_data on client %d with error=%s code=%d", _fd, e.what(), e.code());
+        close(_fd);
 #ifdef DEBUG
         e.dump();
 #endif
